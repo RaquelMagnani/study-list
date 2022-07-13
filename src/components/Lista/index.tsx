a4035d669070baa26924d32c7294d86221cb20d3
Raquel Magnani <quel.magnani88@gmail.com>
2022-07-13 10:22:06 +0200

    aula-4

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Item from "./Item";
 import style from './Lista.module.scss'
 
 function Lista() {
@@ -18,10 +19,12 @@ function Lista() {
         <h2> Estudos do dia </h2>
         <ul>
             {tarefas.map((item, index)=> (
-                <li key={index} className={style.item} >
-                    <h3>{item.tarefa}</h3>
-                    <span>{item.tempo}</span>
-                </li>
+               <Item
+               key={index}
+               tarefa={item.tarefa}
+               tempo={item.tempo}
+               //OU {...item}
+               />
             ))}
           
         </ul>
